04cedb41d09a8a550496406a8088714146ad0fe9
Patrik Karlsson <patrik@trixon.se>
2017-10-28 13:16:53 +0200

    Move quickie to separate module

--- src/main/java/org/nbgames/yaya/PluginWatcher.java
+++ src/main/java/org/nbgames/yaya/PluginWatcher.java
@@ -37,7 +37,7 @@ public class PluginWatcher implements Runnable {
         loadYayaGames();
     }
 
-    private void loadYayaGames() {
+    private synchronized void loadYayaGames() {
         GameTypeLoader.getInstance().init();
     }
 }
